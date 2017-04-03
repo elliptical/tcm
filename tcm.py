42441e8ab9618470c9a8c1b7ecf70acb7e0ffb59
Andrei Boulgakov <andrei.boulgakov@outlook.com>
2017-04-02 21:04:26 -0700

    Require the decorator to use parentheses
    * adjust Pylint config as well in order to allow for long names of the
      test methods
    
    * tweak couple other things to silence Pylint output
    
    The way I check for parentheses is not foolproof, but it should still
    work most of the time.
    
    If you do need to parameterize the decorator with a single callable,
    pass it as a keyword argument.

--- tcm.py
+++ tcm.py
@@ -4,8 +4,18 @@
 ATTR_NAME = 'tcm values'
 
 
+class DecoratorException(Exception):
+    """Exception raised on invalid use of the "values" decorator."""
+
+    pass
+
+
 def values(*args, **kwargs):
     """Create a parameterized decorator which stores its arguments in the decorated object."""
+    if len(args) == 1 and callable(args[0]) and not kwargs:
+        raise DecoratorException(
+            'Invalid use without parentheses or with a callable as the only argument')
+
     def _decorator(func):
         setattr(func, ATTR_NAME, (args, kwargs))
         return func
--- test/test_decorator.py
+++ test/test_decorator.py
@@ -4,6 +4,25 @@ import tcm
 
 
 class DecoratorTestCase(unittest.TestCase):
+    def test_missing_parentheses_will_normally_raise(self):
+        with self.assertRaises(tcm.DecoratorException) as cm:
+            @tcm.values
+            def test():  # pylint: disable=unused-variable
+                pass  # pragma: no cover
+
+        self.assertEqual(
+            cm.exception.args[0],
+            'Invalid use without parentheses or with a callable as the only argument')
+
+    def test_missing_parentheses_will_not_raise_for_determined_users(self):
+        @tcm.values
+        @_zilch
+        def test():
+            pass  # pragma: no cover
+
+        self.assertFalse(hasattr(test, tcm.ATTR_NAME))
+        self.assertEqual(test.__name__, '_decorator')
+
     def test_missing_arguments_are_stored(self):
         @tcm.values()
         def test():
@@ -13,7 +32,7 @@ class DecoratorTestCase(unittest.TestCase):
         self.assertTupleEqual(args, ())
         self.assertDictEqual(kwargs, {})
 
-    def test_single_positional_argument_is_stored(self):
+    def test_single_positional_noncallable_argument_is_stored(self):
         @tcm.values('abc')
         def test():
             pass  # pragma: no cover
@@ -22,7 +41,35 @@ class DecoratorTestCase(unittest.TestCase):
         self.assertTupleEqual(args, ('abc',))
         self.assertDictEqual(kwargs, {})
 
-    def test_single_keyword_argument_is_stored(self):
+    def test_single_positional_callable_argument_will_raise(self):
+        with self.assertRaises(tcm.DecoratorException) as cm:
+            @tcm.values(_dummy_callable)
+            def test():  # pylint: disable=unused-variable
+                pass  # pragma: no cover
+
+        self.assertEqual(
+            cm.exception.args[0],
+            'Invalid use without parentheses or with a callable as the only argument')
+
+    def test_callable_argument_with_another_positional_argument_are_stored(self):
+        @tcm.values(_dummy_callable, 12)
+        def test():
+            pass  # pragma: no cover
+
+        args, kwargs = getattr(test, tcm.ATTR_NAME)
+        self.assertTupleEqual(args, (_dummy_callable, 12))
+        self.assertDictEqual(kwargs, {})
+
+    def test_callable_argument_with_a_keyword_argument_are_stored(self):
+        @tcm.values(_dummy_callable, kw=None)
+        def test():
+            pass  # pragma: no cover
+
+        args, kwargs = getattr(test, tcm.ATTR_NAME)
+        self.assertTupleEqual(args, (_dummy_callable,))
+        self.assertDictEqual(kwargs, {'kw': None})
+
+    def test_single_keyword_noncallable_argument_is_stored(self):
         @tcm.values(kw=None)
         def test():
             pass  # pragma: no cover
@@ -30,6 +77,15 @@ class DecoratorTestCase(unittest.TestCase):
         args, kwargs = getattr(test, tcm.ATTR_NAME)
         self.assertTupleEqual(args, ())
         self.assertDictEqual(kwargs, {'kw': None})
+
+    def test_single_keyword_callable_argument_is_stored(self):
+        @tcm.values(kw=_dummy_callable)
+        def test():
+            pass  # pragma: no cover
+
+        args, kwargs = getattr(test, tcm.ATTR_NAME)
+        self.assertTupleEqual(args, ())
+        self.assertDictEqual(kwargs, {'kw': _dummy_callable})
 
     def test_mixed_arguments_are_stored(self):
         @tcm.values('abc', 12, kw=None)
@@ -39,3 +95,11 @@ class DecoratorTestCase(unittest.TestCase):
         args, kwargs = getattr(test, tcm.ATTR_NAME)
         self.assertTupleEqual(args, ('abc', 12))
         self.assertDictEqual(kwargs, {'kw': None})
+
+
+def _zilch(dummy):
+    return None
+
+
+def _dummy_callable():
+    pass    # pragma: no cover
