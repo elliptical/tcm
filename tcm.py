5106b40f0263b8e8336ef004bcb602af067e350f
Andrei Boulgakov <andrei.boulgakov@outlook.com>
2017-04-29 23:53:47 -0700

    Add basic metaclass implementation

--- tcm.py
+++ tcm.py
@@ -2,6 +2,7 @@
 
 
 from collections import namedtuple
+import functools
 
 
 ATTR_NAME = 'tcm values'
@@ -47,3 +48,62 @@ class values():  # noqa: N801 / pylint: disable=invalid-name,too-few-public-meth
 
 
 _CapturedArguments = namedtuple('CapturedArguments', 'args, kwargs')
+
+
+class TestCaseMeta(type):
+    """Metaclass based runtime generator of the test methods."""
+
+    def __new__(mcs, name, bases, mapping):  # noqa: N804
+        """Create the class after expanding the original mapping."""
+        new_mapping = dict(_expanded_mapping(mapping))
+        return super().__new__(mcs, name, bases, new_mapping)
+
+
+def _expanded_mapping(mapping):
+    """Iterate the mapping while generating new test methods from decorated ones."""
+    for key, value in mapping.items():
+        try:
+            captured_arguments = _get_captured_arguments(value)
+        except AttributeError:
+            # Pass non-decorated items unchanged.
+            yield key, value
+        else:
+            # Use the decorated test method as a sample and generate a distinct
+            # test method for every captured argument.
+            for suffix, arg in _uniformly_named_arguments(captured_arguments):
+                generated = _generate_test_method(value, arg)
+                generated.__name__ = key + '_' + suffix
+                yield generated.__name__, generated
+
+
+def _get_captured_arguments(func):
+    """Raise AttributeError for non-decorated "func", return the captured arguments otherwise."""
+    captured_arguments = getattr(func, ATTR_NAME)
+    if type(captured_arguments) is not _CapturedArguments:  # pylint: disable=unidiomatic-typecheck
+        # The attribute was not set by tcm, so effectively it does not exist.
+        raise AttributeError
+    return captured_arguments
+
+
+def _uniformly_named_arguments(captured_arguments):
+    """Iterate the captured arguments as uniform name/value pairs."""
+    args, kwargs = captured_arguments
+
+    # For positional arguments, the name is 1-based index padded with
+    # leading zeroes to the length of the last index.
+    width = len(str(len(args)))
+    for i, arg in enumerate(args, 1):
+        name = str(i).zfill(width)
+        yield name, arg
+
+    # For keyword arguments, the keyword is taken as a name.
+    yield from kwargs.items()
+
+
+def _generate_test_method(func, arg):
+    """Wrap the original test method by supplying the "arg" as a positional argument."""
+    @functools.wraps(func)
+    def _wrapper(self):
+        return func(self, arg)
+
+    return _wrapper
---
+++ test/test_metaclass.py
@@ -0,0 +1,73 @@
+import unittest
+
+import tcm
+
+
+class MetaclassTestCase(unittest.TestCase):
+    # pylint: disable=no-member
+
+    def test_nondecorated_test_methods_keep_their_names(self):
+        class GeneratedTestCase(unittest.TestCase, metaclass=tcm.TestCaseMeta):
+            def test_no_attr(self):
+                pass  # pragma: no cover
+
+            def test_user_attr(self):
+                pass  # pragma: no cover
+
+            setattr(test_user_attr, tcm.ATTR_NAME, None)
+
+        self.assertFalse(hasattr(GeneratedTestCase.test_no_attr, tcm.ATTR_NAME))
+        self.assertTrue(hasattr(GeneratedTestCase.test_user_attr, tcm.ATTR_NAME))
+
+    def test_decorated_test_methods_are_replaced_with_generated_ones(self):
+        class GeneratedTestCase(unittest.TestCase, metaclass=tcm.TestCaseMeta):
+            @tcm.values(*range(9), kw=-1)
+            def test_few(self, value):
+                self.assertIsInstance(value, int)
+                return value
+
+            @tcm.values(*range(10))
+            def test_many(self, value):
+                self.assertIsInstance(value, int)
+                return value
+
+        self.assertFalse(hasattr(GeneratedTestCase, 'test_few'))
+        self.assertFalse(hasattr(GeneratedTestCase, 'test_many'))
+
+        self.assertEqual(GeneratedTestCase.test_few_1.__name__, 'test_few_1')
+        self.assertEqual(GeneratedTestCase.test_few_9.__name__, 'test_few_9')
+        self.assertEqual(GeneratedTestCase.test_few_kw.__name__, 'test_few_kw')
+        self.assertEqual(GeneratedTestCase.test_many_01.__name__, 'test_many_01')
+        self.assertEqual(GeneratedTestCase.test_many_10.__name__, 'test_many_10')
+
+        gtc = GeneratedTestCase()
+
+        self.assertEqual(gtc.test_few_1(), 0)
+        self.assertEqual(gtc.test_few_9(), 8)
+        self.assertEqual(gtc.test_few_kw(), -1)
+        self.assertEqual(gtc.test_many_01(), 0)
+        self.assertEqual(gtc.test_many_10(), 9)
+
+    def test_incompatible_test_method_signature_will_raise_upon_test_run(self):
+        class GeneratedTestCase(unittest.TestCase, metaclass=tcm.TestCaseMeta):
+            @tcm.values('abc')
+            def test_missing(self, x, y):
+                pass  # pragma: no cover
+
+            @tcm.values('abc')
+            def test_extra(self):
+                pass  # pragma: no cover
+
+        gtc = GeneratedTestCase()
+
+        with self.assertRaises(TypeError) as cm:
+            gtc.test_missing_1()
+        self.assertEqual(
+            cm.exception.args[0],
+            "test_missing() missing 1 required positional argument: 'y'")
+
+        with self.assertRaises(TypeError) as cm:
+            gtc.test_extra_1()
+        self.assertEqual(
+            cm.exception.args[0],
+            'test_extra() takes 1 positional argument but 2 were given')
